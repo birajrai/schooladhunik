d5897e7fa662c15c5eb8f844ae33b9b946c9838e
Biraj Rai <70630924+birajrai@users.noreply.github.com>
2022-11-15 21:51:04 +0545

    init: created: react-router-dom

---
+++ src/components/includes/About.js
@@ -0,0 +1,23 @@
+function About() {
+  return (
+    <div>
+      <span>About</span>
+      <h2>Introduction</h2>
+      <p>
+        We offer a play-based emergent curriculum complete with low teacher to
+        child ratios. Come see our environment. Omega International H.S.S. &amp;
+        College has been suitably located in Lalitpur at Kumaripati in a serene
+        and academic supportive environment over an impressive landscape. Its
+        team is a unique creation of some of Nepal’s well-known educationists,
+        academicians and scholars from diverse subject background required for
+        the disciplines of Science, Management and Humanities. Such team of
+        professionally trained and experienced teacher guides students
+        continuously for competitive theoretical and practical knowledge. There
+        are well equipped Physics, Chemistry, Biology and Computer Laboratories;
+        spacious sports field and sophisticated library for acquiring knowledge
+        and skills.
+      </p>
+    </div>
+  );
+}
+export default About;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//SCSS
+import "./styles/Layout.scss";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Components
+import Navbar from "./components/layouts/Navbar";
+import Home from "./components/pages/Home";
+import Blog from "./components/pages/Blog";
+import Events from "./components/pages/Events";
+import Gallery from "./components/pages/Gallery";
+import Contact from "./components/pages/Contact";
+import NotFound from "./components/pages/NotFound";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route index element={<Home />} />
+          <Route path="blog" element={<Blog />} />
+          <Route path="events" element={<Events />} />
+          <Route path="gallery" element={<Gallery />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
