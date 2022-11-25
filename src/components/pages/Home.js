9f72098c734777bedffb086656e4a72480744301
Biraj Rai <70630924+birajrai@users.noreply.github.com>
2022-11-25 18:23:02 +0545

    init: commit

--- src/components/pages/Home.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-// import Typed from "react-typed";
-const Home = () => {
-  return (
-    <div>Hi</div>
-  )
-};
-
-export default Home;
--- src/components/pages/Results.js
+++
@@ -1,5 +0,0 @@
-const Results = () => {
-  return <h1>Recents Result</h1>;
-};
-
-export default Results;
